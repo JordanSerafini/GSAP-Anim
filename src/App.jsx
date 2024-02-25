217f1200f35ea20b40aa2c0e907d430871d0be3c
JordanSerafini <jordanserafini.74@gmail.com>
2024-02-25 18:21:23 +0100

    :construction: work on daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './index.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className=" ">
-      <button className="btn">Hello daisyUI</button>
+    <div className="bg-white h-screen">
+      <div className=" ">
+        <button className="btn">Hello daisyUI</button>
+      </div>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-error">Button</button>
+      <button className="btn btn-secondary btn-circle">Button</button>
+      <div className="h-3/10 w-10/10 bg-slate-600" >
+      <div className="glass h-8/10 pt-28">Glass</div>
+
+      </div>
+
+
     </div>
   );
 }
